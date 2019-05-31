ed87be9ad0020a6881786642606559617b5b2b2a
Davud <davud@comp.fritz.box>
2019-06-01 01:17:20 +0300

    bug fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,4 +4,3 @@ export const updateNewTaskText = createAction('TEXT_UPDATE');
 export const addTask = createAction('TASK_ADD');
 export const removeTask = createAction('TASK_REMOVE');
 export const toggleTaskState = createAction('TASK_STATE_TOGGLE');
-
