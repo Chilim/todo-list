483a073f79c14e95835cf473920745d5e507fc2b
Davud <davud.kakhriman@gmail.com>
2019-06-02 16:15:58 +0300

    bug fixed

---
+++ src/components/NewTaskForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { updateNewTaskText, addTask } from '../actions';
+
+const mapStateToProps = (state) => {
+  const props = {
+    text: state.text,
+  };
+  return props;
+};
+
+const actionCreators = {
+  updateNewTaskText: updateNewTaskText,
+  addTask: addTask,
+};
+
+class NewTaskForm extends React.Component {
+  handleInput = e => {
+    e.preventDefault();
+    const { updateNewTaskText } = this.props;
+    updateNewTaskText({ text: e.target.value });
+  };
+
+  handleTask = e => {
+    e.preventDefault();
+    const { addTask, text } = this.props;
+    const task = { text, id:_.uniqueId('task_'), state: 'active' };
+    addTask({ task });
+  };
+
+  render() {
+    const { text } = this.props;
+    return (
+      <form action="" className="form-inline" onSubmit={this.handleTask}>
+        <div className="form-group mx-sm-3">
+          <input
+            type="text"
+            required
+            value={text}
+            onChange={this.handleInput}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary btn-sm" value="Add" />
+      </form>
+    )
+  }  
+};
+
+export default connect(mapStateToProps, actionCreators)(NewTaskForm);
