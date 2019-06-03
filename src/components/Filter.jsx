730b85e713a84cd6524b261fa73a6a55a6aa0a9f
Davud <davud.kakhriman@gmail.com>
2019-06-03 03:49:44 +0300

    task filtering logic implemanted

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
+const filters = [['all', 'All Tasks'], ['active', 'Active Tasks'], ['finished', 'Finished Tasks']];
 
-export default class Filter extends React.Component {
+const mapStateToProps = (state) => {
+  const { currentFilterName } = state.tasks;
+  return { currentFilterName };
+}
+
+const actionCreators = {
+  setTasksFilter: actions.setTasksFilter,
+};
+
+class Filter extends React.Component {
+  
+  handleFilter = (filterName) => {
+    const { setTasksFilter } = this.props;
+    setTasksFilter({ filterName });
+  }
+
+  renderBtns = ([filterName, text]) => {
+    const { currentFilterName } = this.props;
+    if (filterName === currentFilterName) {
+      return text;
+    }
+
+    return (
+      <button 
+        type="button"
+        key={filterName}
+        onClick={() => this.handleFilter(filterName)} 
+        className="btn btn-link border-0 p-0" 
+        data-test={`task-filter-${filterName}`}
+        >
+        {text}
+      </button>
+    )
+  }
+
   render() {
     return (
       <div className="mt-3 d-flex justify-content-around">
-        <button type="button" className="btn btn-link border-0 p-0" data-test="task-filter-all">All Tasks</button>
-        <button type="button" className="btn btn-link border-0 p-0" data-test="task-filter-active">Active Tasks</button>
-        <button type="button" className="btn btn-link border-0 p-0" data-test="task-filter-finished">Finished Tasks</button>
+        {filters.map(this.renderBtns)}
       </div>
     );
-  } 
-}
+  }
+} 
+
+export default connect(mapStateToProps, actionCreators)(Filter);
